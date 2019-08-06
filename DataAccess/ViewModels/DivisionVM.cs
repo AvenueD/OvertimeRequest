c057ac39383bf47acb82976b17ce653e6c340f1e
Eko Puji Hatmoko <eko.november06@gmail.com>
2019-08-06 12:12:19 +0700

    Modified Division.cs

--- DataAccess/ViewModels/DivisionVM.cs
+++ DataAccess/ViewModels/DivisionVM.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace DataAccess.ViewModels
 {
-    public class DivisiVM
+    public class DivisionVM
     {
         // Apa yang dibutuhkan di View dan Model, yg diinputkan secara sadar diletakan di ViewModel
         public string Name { get; set; }
-        public DivisiVM() { } // constructor
-        public DivisiVM(string name)
+        public DivisionVM() { } // constructor
+        public DivisionVM(string name)
         {
             this.Name = name;
         }
