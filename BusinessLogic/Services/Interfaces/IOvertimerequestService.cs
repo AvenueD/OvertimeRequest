ff7b2f34467989bc44e0f0e259c455e36ec84a7b
AvenueD <avenuedicky@yahoo.co.id>
2019-08-12 09:31:26 +0700

    modified Services/Interfaces/IOvertimerequestService.cs

--- BusinessLogic/Services/Interfaces/IOvertimerequestService.cs
+++ BusinessLogic/Services/Interfaces/IOvertimerequestService.cs
@@ -13,7 +13,7 @@ namespace BusinessLogic.Services.Interfaces
         List<OvertimeRequest> Get();
         OvertimeRequest Get(int id);
         //List<OvertimeRequest> Get(string value);
-        bool Insert(OvertimeRequestVM overtimerequestVM);
+        bool Insert(OvertimeRequestVM overtimeRequestVM);
         //bool Update(OvertimeRequestVM overtimerequestVM);
         //bool Delete(int id);
     }
